b9ec848b18e9105c73d3fe03fdca89450404ee57
visualstudio <visualstudio@230-PC41>
2019-02-14 11:17:29 -0500

    final

--- C0744421/js/main.js
+++ C0744421/js/main.js
@@ -1,1 +1,11 @@
-﻿// Your code here!
+﻿
+function addNumbers(countUpTo) {
+    sum = 0;
+    for (i = 0; i < countUpTo; i++) {
+        sum += i;
+    }
+    return sum;
+
+}
+var input = prompt("Input your Number!");
+document.write(addNumbers(input));
