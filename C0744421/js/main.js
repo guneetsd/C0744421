89fe571a26ddcf7a1dfb0e49e541286433f182a0
visualstudio <visualstudio@230-PC41>
2019-02-14 11:21:21 -0500

    table

--- C0744421/js/main.js
+++ C0744421/js/main.js
@@ -1,5 +1,5 @@
 ﻿
-function addNumbers(countUpTo) {
+/*function addNumbers(countUpTo) {
     sum = 0;
     for (i = 0; i < countUpTo; i++) {
         sum += i;
@@ -8,4 +8,33 @@ function addNumbers(countUpTo) {
 
 }
 var input = prompt("Input your Number!");
-document.write(addNumbers(input));
+document.write(addNumbers(input));*/
+
+var rows = prompt("How many rows for your multiplicatio table?");
+var cols = prompt("How many columns for your multiplicatio table?");
+if (rows == "" || rows == null)
+    rows = 10;
+if (cols == "" || cols == null)
+    cols = 10;
+createTable(rows, cols);
+function createTable(rows, cols) {
+
+    var j = 1;
+    var output = "<table border ='1' width='500' cellspacing='0' cellpadding ='5'>";
+    for (i = 1; i <= rows; i++) {
+        output = output + "<tr>"
+        while (j <= cols) {
+            output = output + "<td>" + (i * j) + "</td>";
+            j = j + 1;
+        }
+
+        output = output + "</tr>"
+        j = 1;
+
+    }
+
+    output = output + "</table>";
+    document.write(output);
+
+}
+
